bc6c43176c66fbfff7d7139a9c3bd37b211b3358
Andréa Brouart <andrea.brouart@epfedu.fr>
2022-10-25 17:12:03 +0200

    Fonction décapsuler créée

--- TP2_Bieres_BROUART/src/tp2_bieres_brouart/TP2_Bieres_BROUART.java
+++ TP2_Bieres_BROUART/src/tp2_bieres_brouart/TP2_Bieres_BROUART.java
@@ -14,17 +14,23 @@ public class TP2_Bieres_BROUART {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     BouteilleBiere uneBiere = new BouteilleBiere() ;
-     uneBiere.nom = "Cuvee des trolls";
-     uneBiere.degreAlcool = 7.0 ;
-     uneBiere.brasserie = "Dubuisson";
-     uneBiere.ouverte = false;
+     BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0,"Dubuisson", false) ;
+     //uneBiere.nom = "Cuvee des trolls";
+     //uneBiere.degreAlcool = 7.0 ;
+     //uneBiere.brasserie = "Dubuisson";
+     //uneBiere.ouverte = false;
      uneBiere.lireEtiquette(); 
      
-     BouteilleBiere Biere2 = new BouteilleBiere() ;
-     Biere2.nom = "Leffe";
-     Biere2.degreAlcool = 6.6 ;
-     Biere2.brasserie = "Abbaye de Leffe";
+     BouteilleBiere Biere2 = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe",true) ;
      Biere2.lireEtiquette();
+     
+     BouteilleBiere Biere3 = new BouteilleBiere("Goudale",7.2,"Novelty",true);
+     Biere3.lireEtiquette();
+     
+     BouteilleBiere Biere4 = new BouteilleBiere("Chouffe",8.0,"Chez Margaux",false);
+     Biere4.lireEtiquette();
+     
+     BouteilleBiere Biere5 = new BouteilleBiere("Heineken",5.2,"Babar",true);
+     Biere5.lireEtiquette();
 }
 }
