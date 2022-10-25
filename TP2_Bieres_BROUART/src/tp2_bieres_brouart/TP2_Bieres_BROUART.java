dfceb5ab65be4941699c914e70b449aa7b9fdf09
Andréa Brouart <andrea.brouart@epfedu.fr>
2022-10-25 16:22:19 +0200

    Création TP2

---
+++ TP2_Bieres_BROUART/src/tp2_bieres_brouart/TP2_Bieres_BROUART.java
@@ -0,0 +1,20 @@
+/*
+BROUART Andréa TDB
+TP2 25/10/2022
+ */
+package tp2_bieres_brouart;
+
+/**
+ *
+ * @author Asus
+ */
+public class TP2_Bieres_BROUART {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     BouteilleBiere uneBiere = new BouteilleBiere() ;
+    }
+    
+}
