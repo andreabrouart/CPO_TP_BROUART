38b4ebe0ee9d912e904877edec7176ba98edb960
Andréa Brouart <andrea.brouart@epfedu.fr>
2022-10-28 14:44:45 +0200

    Fonction testée et toString ajouté : fini

--- TP2_Bieres_BROUART/src/tp2_bieres_brouart/BouteilleBiere.java
+++ TP2_Bieres_BROUART/src/tp2_bieres_brouart/BouteilleBiere.java
@@ -19,6 +19,7 @@ System.out.println("Bouteille de " + nom +" (" + degreAlcool +
  " degres) \nBrasserie : " + brasserie +"\n ouverte? :" + ouverte) ;
 
 }
+//Création de la fonction BouteilleBiere définissant les caractéristiques de la bouteille
     public BouteilleBiere(String unNom, double unDegre, String
 uneBrasserie, boolean ouverte1) {
  nom = unNom;
@@ -26,13 +27,26 @@ uneBrasserie, boolean ouverte1) {
  brasserie = uneBrasserie;
  ouverte = ouverte1;
     }
+// méthode toString permettant de réecrire les caractéristiques de la bouteille
+ @Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degres)Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+
+    }
+//Création de la fonction afin de savoir si la bouteille est ouverte ou s'il faut l'ouvrir
     public boolean Decapsuler() {
         if (ouverte == false) {
             ouverte = true;
-            return true;
+            System.out.println("La biere est maintenant ouverte"); 
+            return ouverte;
                     }else{
             System.out.println("La biere est deja ouverte"); 
-            return false;
+            ouverte = false;
+            return ouverte;
         }
 }
 }
--- TP2_Bieres_BROUART/src/tp2_bieres_brouart/TP2_Bieres_BROUART.java
+++ TP2_Bieres_BROUART/src/tp2_bieres_brouart/TP2_Bieres_BROUART.java
@@ -14,12 +14,14 @@ public class TP2_Bieres_BROUART {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+     // on définit de deux manières les caractéristiques de uneBiere   
      BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls", 7.0,"Dubuisson", false) ;
      //uneBiere.nom = "Cuvee des trolls";
      //uneBiere.degreAlcool = 7.0 ;
      //uneBiere.brasserie = "Dubuisson";
      //uneBiere.ouverte = false;
-     uneBiere.lireEtiquette(); 
+     System.out.println(uneBiere) ;
+     //uneBiere.lireEtiquette(); 
      
      BouteilleBiere Biere2 = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe",true) ;
      Biere2.lireEtiquette();
@@ -29,11 +31,22 @@ public class TP2_Bieres_BROUART {
      
      BouteilleBiere Biere4 = new BouteilleBiere("Chouffe",8.0,"Chez Margaux",false);
      Biere4.lireEtiquette();
-     Biere4.Decapsuler();
-     Biere4.lireEtiquette();
+     //Biere4.Decapsuler();
+     //Biere4.lireEtiquette();
      
      BouteilleBiere Biere5 = new BouteilleBiere("Heineken",5.2,"Babar",true);
      Biere5.lireEtiquette();
-     Biere5.Decapsuler();
+     //Biere5.Decapsuler();
+     
+     //Derniers tests avec toString et fonction Décapsuler
+     BouteilleBiere Biere6 = new BouteilleBiere("Modelo", 7.4, "Quincallerie", false);
+       System.out.println(Biere6);
+       Biere6.Decapsuler();
+       
+     BouteilleBiere Biere7 = new BouteilleBiere ("1664 blonde", 5.6, "Pinnochio",true);
+      System.out.println(Biere7) ;
+      Biere7.Decapsuler();
+      
+    }
 }
-}
+       
