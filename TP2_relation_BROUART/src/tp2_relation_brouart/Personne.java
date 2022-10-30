a29360fd493c289e0e36942c0262e8c5bb9a7a4f
Andréa Brouart <andrea.brouart@epfedu.fr>
2022-10-30 17:51:16 +0100

    création voiture_a_ajouter finie

--- TP2_relation_BROUART/src/tp2_relation_brouart/Personne.java
+++ TP2_relation_BROUART/src/tp2_relation_brouart/Personne.java
@@ -23,4 +23,22 @@ String chaine_a_retourner;
 chaine_a_retourner = ( nom + prenom );
 return chaine_a_retourner ;
 }
+public boolean ajouter_voiture( Voiture voiture_a_ajouter) {
+        if (voiture_a_ajouter.proprietaire !=null){
+            System.out.println(voiture_a_ajouter + "Voiture volee !!!");
+            return false;
+            //Si la voiture à ajouter possède déjà un propriétaire on retourne "voiture volée !!!")
+            }else{
+            if(this.nbVoitures == 3){
+                System.out.println(this.nom + this.prenom+ "a déjà 3 voitures");
+                return false;
+                //Si le propriétaire a déjà 3 voitures, le tableau est plein, on retourne "le propriétaire a déjà 3 voitures".
+            }else{
+                this.liste_voitures[nbVoitures] = voiture_a_ajouter;
+                nbVoitures += 1;
+                voiture_a_ajouter.proprietaire = this  ;
+                return true;
+                // on indique à la voiture son propriétaire 
+                }
+            }
 }
